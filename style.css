d3e0bfde798d7c5cc20427a68b9e979271ca8ced
NotAd4m <148502805+NotAd4m@users.noreply.github.com>
2024-01-29 20:16:36 +0100

    Add files via upload

---
+++ style.css
@@ -0,0 +1,28 @@
+html {
+    size: 100dvh;
+}
+
+body {
+    background: linear-gradient(to right bottom ,rgb(77, 4, 4), rgb(14, 0, 0));
+    background-repeat: no-repeat;
+    background-size: contain;
+}
+
+div {
+    width: 75dvh;
+    height: 50dvh;
+    border: 2px solid white;
+    margin: 24.76dvh auto;
+    background-color: rgb(8, 8, 8);
+    background-image: url("basicbgblack.png");
+    background-repeat: no-repeat;
+    background-position: center;
+}
+
+#main {
+  color: white;  
+}
+
+#second {
+    color: white;  
+  }
